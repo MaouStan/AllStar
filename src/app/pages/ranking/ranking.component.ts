452898ec9ec96aca780483fc7d29e5b0a9f48940
blzniti <beelazy1579@gmail.com>
2024-02-28 11:51:47 +0700

    ranking

--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { Card1Component } from '../../components/card-1/card-1.component';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [NavbarComponent, Card1Component, ],
+  imports: [NavbarComponent ],
   templateUrl: './ranking.component.html',
   styleUrl: './ranking.component.scss',
 })
